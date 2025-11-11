608250735f63fc87f81ef9c05c97d9829707d250
ShinobuTakahashi <a.little.more.than.before@gmail.com>
2025-11-11 17:05:17 +0900

    Add src

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import * as core from '@actions/core';
+import { execSync } from "child_process";
+
+const inputs = {
+	beforeDay: core.getInput("before_day")
+};
+
+const BEFORE_DAYS = 7;
+
+function makeBeforeDateValue(beforeDays: number): string {
+    const dt = new Date();
+    dt.setDate(dt.getDate() - beforeDays);
+    const beforeDate = dt.toLocaleDateString("ja-JP", {year: "numeric", month: "2-digit", day: "2-digit"}).replaceAll('/', '-');
+    return beforeDate;
+}
+
+try {
+    const beforDay = inputs.beforeDay ? parseInt(inputs.beforeDay) : BEFORE_DAYS;
+    const beforeDate = makeBeforeDateValue(beforDay);
+    const npmInstallCmd = `npm i --before ${beforeDate}`;
+    console.log(`- exec: "${npmInstallCmd}"`);
+    execSync(npmInstallCmd);
+
+    const gitStatusCmd = "git status --porcelain";
+    const result = execSync(gitStatusCmd);
+    console.log("******[", result.toString(),"]", result.toString().length);
+    if (result.toString().length > 0) { 
+        console.log(`- npm install results in changes! Run "${npmInstallCmd}" to update package-lock.json`);
+        process.exit(1);
+    }
+    console.log("--- end ---");
+} catch (e) { 
+    console.log("- error:", e);
+}
+
+
+
+
