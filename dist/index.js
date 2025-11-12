f51d340cc5297efa97fb2fc2ed1b96c11b1f8d61
ShinobuTakahashi <a.little.more.than.before@gmail.com>
2025-11-12 18:36:54 +0900

    Fix index.ts

--- dist/index.js
+++ dist/index.js
@@ -1,31 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const core = require("@actions/core");
 const child_process_1 = require("child_process");
+const core = require("@actions/core");
 const inputs = {
-    beforeDay: core.getInput("before_day")
+    beforeDays: core.getInput("before_days")
 };
 const BEFORE_DAYS = 7;
+/**
+ * 今日日付から引数で渡された n 日前の日付を "yyyy-mm-dd" 形式の文字列へ変換し返す。
+ */
 function makeBeforeDateValue(beforeDays) {
     const dt = new Date();
     dt.setDate(dt.getDate() - beforeDays);
-    const beforeDate = dt.toLocaleDateString("ja-JP", { year: "numeric", month: "2-digit", day: "2-digit" }).replaceAll('/', '-');
+    const beforeDate = dt.toLocaleDateString("ja-JP", { year: "numeric", month: "2-digit", day: "2-digit" }).replaceAll("/", "-");
     return beforeDate;
 }
 try {
-    const beforDay = inputs.beforeDay ? parseInt(inputs.beforeDay) : BEFORE_DAYS;
+    const beforDay = inputs.beforeDays ? parseInt(inputs.beforeDays, 10) : BEFORE_DAYS;
     const beforeDate = makeBeforeDateValue(beforDay);
     const npmInstallCmd = `npm i --before ${beforeDate}`;
     console.log(`- exec: "${npmInstallCmd}"`);
     (0, child_process_1.execSync)(npmInstallCmd);
     const gitStatusCmd = "git status --porcelain";
     const result = (0, child_process_1.execSync)(gitStatusCmd);
-    console.log("******[", result.toString(), "]", result.toString().length);
-    if (result.toString().length > 0) {
+    console.log(`- result: ${result.toString()}`);
+    if (result.toString()) {
         console.log(`- npm install results in changes! Run "${npmInstallCmd}" to update package-lock.json`);
         process.exit(1);
     }
-    console.log("--- end ---");
 }
 catch (e) {
     console.log("- error:", e);
--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,9 @@ const inputs = {
 
 const BEFORE_DAYS = 7;
 
+/**
+ * 今日日付から引数で渡された n 日前の日付を "yyyy-mm-dd" 形式の文字列へ変換し返す。
+ */
 function makeBeforeDateValue(beforeDays: number): string {
 	const dt = new Date();
 	dt.setDate(dt.getDate() - beforeDays);
@@ -17,22 +20,18 @@ function makeBeforeDateValue(beforeDays: number): string {
 try {
 	const beforDay = inputs.beforeDays ? parseInt(inputs.beforeDays, 10) : BEFORE_DAYS;
 	const beforeDate = makeBeforeDateValue(beforDay);
+
 	const npmInstallCmd = `npm i --before ${beforeDate}`;
 	console.log(`- exec: "${npmInstallCmd}"`);
 	execSync(npmInstallCmd);
 
 	const gitStatusCmd = "git status --porcelain";
 	const result = execSync(gitStatusCmd);
-	console.log("******[", result.toString(), "]", result.toString().length);
-	if (result.toString().length > 0) {
+	console.log(`- result: ${result.toString()}`);
+	if (result.toString()) {
 		console.log(`- npm install results in changes! Run "${npmInstallCmd}" to update package-lock.json`);
 		process.exit(1);
 	}
-	console.log("--- end ---");
 } catch (e) {
 	console.log("- error:", e);
 }
-
-
-
-
